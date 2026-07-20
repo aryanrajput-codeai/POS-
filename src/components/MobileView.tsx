5a8f35be51d9fc86923bd52d2e3576bc82667c32
aryanrajput-codeai <aiaryanrajput@gmail.com>
2026-07-20 20:24:09 +0530

    Update MobileView.tsx

--- src/components/MobileView.tsx
+++ src/components/MobileView.tsx
@@ -29,7 +29,7 @@ import {
 } from "lucide-react";
 import { motion, AnimatePresence } from "motion/react";
 import { MenuItem, CartItem, Category } from "../types";
-import { categories as defaultCategories } from "../Data";
+import { categories as defaultCategories } from "../menuData";
 import { LocalDB } from "../lib/db";
 import TableFloorplan from "./TableFloorplan";
 
