39b9ff080df8bc045f398be1befc7d617c9401a0
aryanrajput-codeai <aiaryanrajput@gmail.com>
2026-07-20 20:12:53 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { MenuItem, Review, KOT, KOTStatus, OrderItem, RestaurantTable, PrinterEmulatorLog, Category } from "../types";
-import { menuItems as defaultMenuItems, reviews as defaultReviews, categories as defaultCategories } from "../data";
+import { menuItems as defaultMenuItems, reviews as defaultReviews, categories as defaultCategories } from "../Data";
 
 const dummyChain: any = {
   from: () => dummyChain,
