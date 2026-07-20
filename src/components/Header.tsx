722175bd8f03f40f592643781e3656db476ae149
aryanrajput-codeai <aiaryanrajput@gmail.com>
2026-07-20 20:23:45 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Search, Sparkles, Flame, Check, ThumbsUp, Leaf } from "lucide-react";
 import { motion } from "motion/react";
-import { categories } from "../Data";
+import { categories } from "../menuData";
 import { Category } from "../types";
 
 interface HeaderProps {
